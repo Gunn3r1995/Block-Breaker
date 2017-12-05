99ec0d472957582520745e93460a565786bb9978
Shane Smith <srs23@brighton.ac.uk>
2017-12-05 15:17:11 +0000

    inital commit

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MusicPlayer : MonoBehaviour
+    {
+        private static MusicPlayer _instance = null;
+
+        // Use this for initialization
+        void Start()
+        {
+            if (_instance)
+            {
+                Destroy(gameObject);
+                Debug.Log("Destroyed duplicate music player");
+            }
+            else
+            {
+                _instance = this;
+                GameObject.DontDestroyOnLoad(gameObject);
+            }
+        }
+    }
+}
